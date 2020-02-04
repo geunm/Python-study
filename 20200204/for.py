ea907dc9b5150c82a5628cd2b0231e514570e293
geunm <kimgeun3182@gmail.com>
2020-02-04 19:55:09 +0900

    for list

---
+++ 20200204/for.py
@@ -0,0 +1,6 @@
+numbers=[1,2,3,4,5]
+universities=['SNU', 'KAIST']
+for number in numbers:
+    print(number)
+for university in universities:
+    print(university)
---
+++ 20200204/list.py
@@ -0,0 +1,4 @@
+numbers=[1,2,3,4,5]
+universities=['SNU', 'KAIST']
+print(numbers[0])
+print(universities[-1])
