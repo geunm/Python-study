46352b251c887c2b6fdab4b071dade2d3628337e
geunm <kimgeun3182@gmail.com>
2020-02-08 15:27:50 +0900

    if def

---
+++ 20200208/def.py
@@ -0,0 +1,8 @@
+def add3(a):
+    res = a + 3
+    res = res * 2
+    print(res)
+    
+add3(5)       
+add3(8)
+add3(100)
---
+++ 20200208/if.py
@@ -0,0 +1,12 @@
+a=5
+if a<3:
+    print(1)
+elif a<7:
+    print(2)
+else:
+    print(3)
+a=8
+if 3 <a and a<7:
+    print(1)
+else:
+    print(2)
